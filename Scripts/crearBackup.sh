534d2115b9ad0aa16c5b8ad1f10eacb3a75ed9db
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2023-05-21 16:10:17 +0200

    .

--- Scripts/crearBackup.sh
+++ Scripts/crearBackup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+read -p "MySQL Password: " pass
+
 #Crear la carpeta
 fecha=$(date +%Y-%m-%d)
 DIR=$HOME/Backups/Backup-$fecha
 mkdir $DIR
 
 #Copia completa de MYSQL
-mysqldump -uroot -pxabier2001 --all-databases --single-transaction --events > myBackup.sql
+mysqldump -uroot -p$pass --all-databases --single-transaction --events > myBackup.sql
 mv myBackup.sql $DIR
 
 #Copia de mysqld.cnf
 sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf $DIR
 
-#Crear el zip de la carpeta
+#Crear el zip de la carpeta y lo mete en el servidor Apache 
+#para poder descargarlo desde otro ordenador
 zip -r Backup-$fecha.zip $DIR
 sudo mv Backup-$fecha.zip /var/www/html/
--- Scripts/downloadAnonfile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read -p "URL: " url
-
-durl=$(curl -s $url | awk -v RS='<' '/id="download-url"/{print gensub(/^.*href="([^"]*)".*$/, "\\1","g", $0)}')
-
-wget --referer=https://anonfiles.com/ $durl
